6dd67514433b2ee65d81e5c8c31902f57c37f04d
Gary Alarcon <gary_pc15_2@hotmail.com>
2011-11-30 14:23:05 -0600

    proyecto

---
+++ src/Facebook.java
@@ -0,0 +1,175 @@
+import java.util.StringTokenizer;
+import java.io.RandomAccessFile;
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+public class Facebook {
+
+    /**
+* @param args the command line arguments
+*/
+    
+    
+// File profile = new File("profile.fbn");
+    static File fb= new File("Facebook.java");
+    
+    public boolean seekEmail(String c){
+       try{
+           //File gerencia= new File();
+           RandomAccessFile rafc = new RandomAccessFile("gerencia.fbn","rw");
+           
+           rafc.seek(0);
+           while(rafc.getFilePointer()<rafc.length()){
+               String cc=rafc.readUTF();
+               rafc.readUTF();
+               rafc.readBoolean();
+               //rafc.readLong();
+               if(cc.equals(c)){
+                   System.out.println("huy");
+                   return false;
+               }
+           }
+           
+       }catch(Exception e){
+           System.out.println(e);
+           return false;
+       }
+       return true;
+    }
+    
+   public boolean seefFriend(String e,String fe){
+        try{
+            RandomAccessFile rafs = new RandomAccessFile(path()+e+"\\"+"manageFriends.fbn","rw");
+            rafs.seek(0) ;
+            while(rafs.getFilePointer()<rafs.length()){
+                String cef=rafs.readUTF();
+                rafs.readBoolean();
+                rafs.readBoolean();
+                if(cef.equals(fe)){
+                    return false;
+                }
+            }
+        }catch(Exception q ){
+            
+            return true;
+        }
+        return true;
+   }
+   
+   
+ 
+    public  String path(){
+        StringTokenizer st= new StringTokenizer(fb.getAbsolutePath(),"\\");
+        String path="";
+        while(st.hasMoreTokens()){
+            String temp=st.nextToken();
+            if(!"Facebook.java".equals(temp)){
+                path+=temp+"\\";
+            }
+        }
+        return path;
+    }
+    
+    public boolean addUser(String n, String p,char g,long d,String c,int t){
+        try{
+            RandomAccessFile rafau= new RandomAccessFile("gerencia.fbn","rw");
+            File carpeta = new File(c);
+           
+            if(carpeta.mkdir()){
+             
+            if(seekEmail(c)){
+                RandomAccessFile rafaud= new RandomAccessFile(path()+c+"\\"+"profile.fbn","rw");
+                RandomAccessFile rafaf= new RandomAccessFile(path()+c+"\\"+"manageFriends.fbn","rw");
+                RandomAccessFile raff = new RandomAccessFile(path()+c+"\\"+"foto.fbn","rw");
+                RandomAccessFile rafp = new RandomAccessFile(path()+c+"\\"+"fotop.fbn","rw");
+                rafau.seek(rafau.length());
+                rafau.writeUTF(c);
+                rafau.writeUTF(p);
+                rafau.writeBoolean(true);
+                    rafaud.writeUTF(n);
+                    rafaud.writeChar(g);
+                    rafaud.writeLong(d);
+                    rafaud.writeInt(t);
+                    Date dc= new Date();
+                    rafaud.writeLong(dc.getTime());
+                        rafp.writeUTF("/silueta.jpg");
+                    return true;
+                }
+                
+            }
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+            return false;
+        }
+        return false;
+    }
+    
+    public boolean addFriends(String cpropip,String camigo ){
+        try{
+            
+            if(!seekEmail(camigo)){
+                RandomAccessFile rafa = new RandomAccessFile(path()+camigo+"\\"+"manageFriends.fbn","rw");
+            RandomAccessFile rafp = new RandomAccessFile(path()+cpropip+"\\"+"manageFriends.fbn","rw");
+            rafa.seek(rafa.length());
+            rafa.writeUTF(cpropip);
+            rafa.writeBoolean(false);
+            rafa.writeBoolean(false);
+            rafp.seek(rafp.length());
+            rafp.writeUTF(camigo);
+            rafp.writeBoolean(false);
+            rafp.writeBoolean(false);
+            return true;
+            }
+            
+                    }catch(Exception e){
+                        return false;
+                    }
+        return false;
+    }
+    
+    public boolean addComment(String e,String c){
+        try{
+            RandomAccessFile rafc = new RandomAccessFile(path()+c+"profile.fbn","rw");
+            rafc.seek(rafc.length());
+            rafc.writeUTF(c);
+        }catch(Exception ex){
+            return false;
+        }
+        return false;
+    }
+    
+    public boolean login(String e,String p){
+        try{
+            RandomAccessFile rafl = new RandomAccessFile("gerencia.fbn", "rw");
+           rafl.seek(0);
+            while(rafl.getFilePointer()<rafl.length()){
+                String ev=rafl.readUTF();
+                String pv=rafl.readUTF();
+                rafl.readBoolean();
+                
+                if(ev.equals(e) && pv.equals(p) )
+                {
+                    return true;
+                }
+                
+            }
+            return false;
+        }
+        catch(IOException a){
+            System.out.println(a.getMessage()+" "+a);
+        }
+        System.out.println("ouch");
+        return false;
+    }
+    
+   
+    
+//
+// // public static void main(String[] args) {
+// // TODO code application logic here
+//
+// addUser();
+// }
+
+
+}
---
+++ src/dire.java
@@ -0,0 +1,18 @@
+
+import java.io.File;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Gary
+ */
+public class dire {
+    static File p = new File("dire.java");
+public static void main(String arg[]){
+    System.out.println(p.getAbsolutePath());     
+}
+}
